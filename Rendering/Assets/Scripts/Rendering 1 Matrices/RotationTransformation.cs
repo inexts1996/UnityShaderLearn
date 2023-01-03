06a0d78594891e58b8eeb31b52983f257338c586
zhouzigui <zhouzigui@bettagames.com>
2023-01-03 08:00:42 +0800

    完善：旋转矩阵

--- Rendering/Assets/Scripts/Rendering 1 Matrices/RotationTransformation.cs
+++ Rendering/Assets/Scripts/Rendering 1 Matrices/RotationTransformation.cs
@@ -8,6 +8,13 @@ namespace Assets.Scripts.Rendering_1_Matrices
 
         public override Vector3 Apply(Vector3 point)
         {
+            float radX = _rotation.x * Mathf.Deg2Rad;
+            float sinX = Mathf.Sin(radX);
+            float cosX = Mathf.Cos(radX); 
+
+            float radY = _rotation.y * Mathf.Deg2Rad;
+            float sinY = Mathf.Sin(radY);
+            float cosY = Mathf.Cos(radY); 
             float radZ = _rotation.z * Mathf.Deg2Rad;
             float sinZ = Mathf.Sin(radZ);
             float cosZ = Mathf.Cos(radZ);
