49100b9ac850012f710b83a495081eb30f093aa7
zhouzigui <zhouzigui@bettagames.com>
2023-01-10 07:00:34 +0800

    添加：投影变换

--- Rendering/Assets/Scripts/Rendering 1 Matrices/CameraTransformation.cs
+++ Rendering/Assets/Scripts/Rendering 1 Matrices/CameraTransformation.cs
@@ -4,15 +4,16 @@ namespace Assets.Scripts.Rendering_1_Matrices
 {
     internal class CameraTransformation : Transformation
     {
+        [SerializeField] private float _focalLength = 1.0f;
         public override Matrix4x4 Matrix
         {
             get
             {
                 var matrix = new Matrix4x4();
-                matrix.SetRow(0, new Vector4(1, 0, 0, 0));
-                matrix.SetRow(1, new Vector4(0, 1, 0, 0));
+                matrix.SetRow(0, new Vector4(_focalLength, 0, 0, 0));
+                matrix.SetRow(1, new Vector4(0, _focalLength, 0, 0));
                 matrix.SetRow(2, new Vector4(0, 0, 0, 0));
-                matrix.SetRow(3, new Vector4(0, 0, 0, 1));
+                matrix.SetRow(3, new Vector4(0, 0, 1, 0));
                 return matrix;
             }
         }
--- Rendering/Assets/Scripts/Rendering 1 Matrices/PositionTransformation.cs
+++ Rendering/Assets/Scripts/Rendering 1 Matrices/PositionTransformation.cs
@@ -11,9 +11,9 @@ namespace Assets.Scripts.Rendering_1_Matrices
             get
             {
                 var matrix = new Matrix4x4();
-                matrix.SetRow(0, new Vector4(0, 0, 0, _offsetPosition.x));
-                matrix.SetRow(1, new Vector4(0, 0, 0, _offsetPosition.y));
-                matrix.SetRow(2, new Vector4(0, 0, 0, _offsetPosition.z));
+                matrix.SetRow(0, new Vector4(1, 0, 0, _offsetPosition.x));
+                matrix.SetRow(1, new Vector4(0, 1, 0, _offsetPosition.y));
+                matrix.SetRow(2, new Vector4(0, 0, 1, _offsetPosition.z));
                 matrix.SetRow(3, new Vector4(0, 0, 0, 1));
 
                 return matrix;
